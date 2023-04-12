9678d62a90277510a29feeff86b720a9a657f019
Marouan Mohamed <medmarouen@gmail.com>
2023-04-12 14:33:40 +0200

    .

--- pkg/collectors/wmbusmeters.go
+++ pkg/collectors/wmbusmeters.go
@@ -2,6 +2,8 @@ package collectors
 
 import (
 	"encoding/json"
+	"fmt"
+	"bytes"
 
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
@@ -17,45 +19,6 @@ const (
 	heatTemperatureReturnKey = "heat_temperature_return"
 )
 
-var (
-	waterCubicUsed = prometheus.NewDesc(
-		waterCubicUsedKey,
-		"Cubic meters of water used",
-		[]string{"id", "name", "meter", "timestamp", "prefix", "serial_number", "current_alarms", "previous_alarms"}, nil,
-	)
-	heatKwhConsumption = prometheus.NewDesc(
-		heatKwhConsumptionKey,
-		"KWH Heat consumption",
-		[]string{"id", "name", "meter", "timestamp"}, nil,
-	)
-	heatCubicConsumption = prometheus.NewDesc(
-		heatCubicConsumptionKey,
-		"Cubic Heat consumption",
-		[]string{"id", "name", "meter", "timestamp"}, nil,
-	)
-	heatCubicFlow = prometheus.NewDesc(
-		heatCubicFlowKey,
-		"Cubic Heat flow",
-		[]string{"id", "name", "meter", "timestamp"}, nil,
-	)
-	heatKwhFlow = prometheus.NewDesc(
-		heatKwhFlowKey,
-		"KWH Heat flow",
-		[]string{"id", "name", "meter", "timestamp"}, nil,
-	)
-	heatTemperatureFlow = prometheus.NewDesc(
-		heatTemperatureFlowKey,
-		"Celsius Heat flow temperature",
-		[]string{"id", "name", "meter", "timestamp"}, nil,
-	)
-	heatTemperatureReturn = prometheus.NewDesc(
-		heatTemperatureReturnKey,
-		"Celsius Heat return temperature",
-		[]string{"id", "name", "meter", "timestamp"}, nil,
-	)
-	
-)
-
 type wmbusmetersCollector struct {
 	inMemoriesData map[string]string
 }
@@ -67,20 +30,38 @@ func NewWMBusmetersCollector(inMemoriesData map[string]string) prometheus.Collec
 
 // Describe implements the prometheus.Collector interface.
 func (collector *wmbusmetersCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- waterCubicUsed
+
+}
+
+// Helper to check element in slice
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
 
 // Collect implements the prometheus.Collector interface.
 func (collector *wmbusmetersCollector) Collect(ch chan<- prometheus.Metric) {
 
-	addWaterGauge := func(desc *prometheus.Desc, data map[string]interface{}, v float64, lv ...string) {
-		lv = append([]string{data["id"].(string), data["name"].(string), data["meter"].(string), data["timestamp"].(string), data["prefix"].(string), data["serial_number"].(string), data["current_alarms"].(string), data["previous_alarms"].(string)}, lv...)
-		ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, v, lv...)
-	}
+	addGauge := func(key string, help string, data map[string]interface{}, v float64) {
+		labelValues := []string{"total_m3", "total_energy_consumption_kwh", "total_volume_m3", "volume_flow_m3h", "power_kw", "flow_temperature_c", "return_temperature_c"}
+		labels := []string{"id", "name", "meter", "timestamp"}
+		values := []string{data["id"].(string), data["name"].(string), data["meter"].(string), data["timestamp"].(string)}
 
-	addHeatGauge := func(desc *prometheus.Desc, data map[string]interface{}, v float64, lv ...string) {
-		lv = append([]string{data["id"].(string), data["name"].(string), data["meter"].(string), data["timestamp"].(string)}, lv...)
-		ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, v, lv...)
+		for key, value := range data {
+			if !contains(labels, key) && !contains(labelValues, key) {
+				var temp_buff bytes.Buffer
+				fmt.Fprintf(&temp_buff, "%v", value)
+				labels = append(labels, key)
+				values = append(values, temp_buff.String())
+			} 
+		}
+		
+		desc := prometheus.NewDesc(key, help, labels, nil,)
+		ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, v, values...)
 	}
 
 	count := 0
@@ -90,18 +71,18 @@ func (collector *wmbusmetersCollector) Collect(ch chan<- prometheus.Metric) {
 		json.Unmarshal([]byte(metric), &parsedData)
 
 		if parsedData["media"].(string) == "water" {
-			addWaterGauge(waterCubicUsed, parsedData, parsedData["total_m3"].(float64))
+			addGauge(waterCubicUsedKey, "Cubic meters of water used", parsedData, parsedData["total_m3"].(float64))
 			delete(collector.inMemoriesData, id)
 			count++
 		}
 
 		if parsedData["media"].(string) == "heat" {
-			addHeatGauge(heatKwhConsumption, parsedData, parsedData["total_energy_consumption_kwh"].(float64))
-			addHeatGauge(heatCubicConsumption, parsedData, parsedData["total_volume_m3"].(float64))
-			addHeatGauge(heatCubicFlow, parsedData, parsedData["volume_flow_m3h"].(float64))
-			addHeatGauge(heatKwhFlow, parsedData, parsedData["power_kw"].(float64))
-			addHeatGauge(heatTemperatureFlow, parsedData, parsedData["flow_temperature_c"].(float64))
-			addHeatGauge(heatTemperatureReturn, parsedData, parsedData["return_temperature_c"].(float64))
+			addGauge(heatKwhConsumptionKey, "KWH Heat consumption", parsedData, parsedData["total_energy_consumption_kwh"].(float64))
+			addGauge(heatCubicConsumptionKey, "Cubic Heat consumption", parsedData, parsedData["total_volume_m3"].(float64))
+			addGauge(heatCubicFlowKey, "Cubic Heat flow", parsedData, parsedData["volume_flow_m3h"].(float64))
+			addGauge(heatKwhFlowKey, "KWH Heat flow", parsedData, parsedData["power_kw"].(float64))
+			addGauge(heatTemperatureFlowKey, "Celsius Heat flow temperature", parsedData, parsedData["flow_temperature_c"].(float64))
+			addGauge(heatTemperatureReturnKey, "Celsius Heat return temperature", parsedData, parsedData["return_temperature_c"].(float64))
 			delete(collector.inMemoriesData, id)
 			count++
 		}
